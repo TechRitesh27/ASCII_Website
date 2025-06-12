0769ef197de8d506940385367ab7b14cb5d7a6fa
Ritesh <riteshukade27@gmail.com>
2025-06-12 18:46:21 +0530

    Navbar case sensitive issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
-import "../styles/navbar.css";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   const location = useLocation();
